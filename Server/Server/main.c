22760e7929f771296370573382622dee85632154
Carlos Pinho <a21220528@isec.pt>
2018-04-19 17:47:45 +0100

    Threads Spaceship defender

--- Server/Server/main.c
+++ Server/Server/main.c
@@ -1,6 +1,92 @@
 #include "../../Shared/sharedHeader.h"
 
+#define NUM_THREADS 3
+
+int random(int min, int max) {
+	return(min + rand() % (max - min));
+}
+
+HANDLE hMutex;
+
+DWORD WINAPI checkDefenderSpaceshop(LPVOID param);
+void goToXY(int x, int y);
+
 int _tmain(int argc, LPTSTR argv[]) {
+#ifdef UNICODE
+	_setmode(_fileno(stdin), _O_WTEXT);
+	_setmode(_fileno(stdout), _O_WTEXT);
+#endif // UNICODE
+
+	DWORD * threadDefenderSpaceshipID;
+	HANDLE * hThreadDefenderSpaceship;
+	SPACESHIP * defenderSpaceship = NULL;
+
+	srand((unsigned int) time(NULL));
+
+	threadDefenderSpaceshipID = (DWORD *)malloc(sizeof(DWORD) * NUM_THREADS);
+	hThreadDefenderSpaceship = (HANDLE *)malloc(sizeof(HANDLE) * NUM_THREADS);
+	defenderSpaceship = (SPACESHIP *)malloc(sizeof(SPACESHIP) * NUM_THREADS);
+
+	hMutex = CreateMutex(NULL, FALSE, NULL);
+
+	for (int i = 0; i < NUM_THREADS; i++) {
+		defenderSpaceship[i].id = i;
+		defenderSpaceship[i].spaceshipType = SPACESHIP_DEFENDER_TYPE;
+		defenderSpaceship[i].sizeX = 3;
+		defenderSpaceship[i].sizeY = defenderSpaceship[i].sizeX;
+		defenderSpaceship[i].lifePoints = 1;
+		defenderSpaceship[i].resistancePoints = 1;
+		defenderSpaceship[i].cordX = random(0, 20);
+		defenderSpaceship[i].cordY = random(0, 20);
+
+		hThreadDefenderSpaceship[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)checkDefenderSpaceshop, (LPVOID)&defenderSpaceship[i], 0, &threadDefenderSpaceshipID);
+		if (hThreadDefenderSpaceship[i] != NULL) {
+			//_tprintf(TEXT("Lançei uma thread com id %d.\n"), (int) threadDefenderSpaceshipID);
+		}else {
+			_tprintf(TEXT("Erro ao criar Thread.\n"));
+		}
+	}
+
+	WaitForMultipleObjects(NUM_THREADS, hThreadDefenderSpaceship, TRUE, INFINITE);
+
+	CloseHandle(hMutex);
+
+	free(defenderSpaceship);
+	free(hThreadDefenderSpaceship);
+	free(threadDefenderSpaceshipID);
+	_gettch();
+	_tprintf(TEXT("Vou terminar...\n"));
 
 	return 0;
+}
+
+DWORD WINAPI checkDefenderSpaceshop(LPVOID param) {
+	HANDLE hSem;
+	SPACESHIP *tempSpaceship = (SPACESHIP *)param;
+
+	//_tprintf(TEXT("[Thread %d] >> Vou Começar a trabalhar!\n"), GetCurrentThreadId());
+
+	for (int i = 0; i < 30; i++) {
+		hSem = CreateSemaphore(NULL, 1, 1, TEXT("teste"));
+		WaitForSingleObject(hSem, INFINITE);
+		goToXY(tempSpaceship->cordX, tempSpaceship->cordY);
+		tempSpaceship->cordX++;
+		Sleep(100);
+		_tprintf(TEXT("%d"), tempSpaceship->id);
+		ReleaseSemaphore(hSem, 1, NULL);
+		CloseHandle(hSem);
+	}
+
+	return 0;
+}
+
+void goToXY(int x, int y) {
+	static HANDLE hStdout = NULL;
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	if (!hStdout) {
+		hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	}
+	SetConsoleCursorPosition(hStdout, coord);
 }
--- Shared/sharedHeader.h
+++ Shared/sharedHeader.h
@@ -1,31 +1,38 @@
-#include <tchar.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <Windows.h>
+#include <tchar.h>
+#include <io.h>
+#include <fcntl.h>
 
-#define SPACESHIP_DEFENDER_TYPE 0;
-#define SPACESHIP_ENEMY_TYPE_BASIC 1;
-#define SPACESHIP_ENEMY_TYPE_DODGE 2;
-#define SPACESHIP_ENEMY_TYPE_SLOW 3;
+#define	SPACESHIP_DEFENDER_TYPE		0x00;
+#define SPACESHIP_ENEMY_TYPE_BASIC	0x01;
+#define SPACESHIP_ENEMY_TYPE_DODGE	0x02;
+#define SPACESHIP_ENEMY_TYPE_SLOW	0x03;
 
-typedef struct Spaceship {
+typedef struct SPACESHIP {
 	int id;
 	int spaceshipType;
 	int lifePoints;
 	int resistancePoints;
 	int cordX, cordY;
-}Spaceship;
+	int sizeX, sizeY;
+}SPACESHIP;
 
-#define POWERUP_TYPE_SHILD 0;
-#define POWERUP_TYPE_ICE 1;
-#define POWERUP_TYPE_BATTERY 2;
-#define POWERUP_TYPE_PLUS 3;
-#define POWERUP_TYPE_LIFE 4;
-#define POWERUP_TYPE_ALCOOL 5;
-#define POWERUP_TYPE_EXTRA 6;
+#define POWERUP_TYPE_SHILD		0x00;
+#define POWERUP_TYPE_ICE		0x01;
+#define POWERUP_TYPE_BATTERY	0x02;
+#define POWERUP_TYPE_PLUS		0x03;
+#define POWERUP_TYPE_LIFE		0x04;
+#define POWERUP_TYPE_ALCOOL		0x05;
+#define POWERUP_TYPE_EXTRA		0x06;
 
-typedef struct Powerup {
+typedef struct POWERUP {
 	int id;
 	int powerupType;
 	int spawnOccurrence;
 	int durantion;
 	int cordX, cordY;
-}Powerup;
+	int sizeX, sizeY;
+}POWERUP;
